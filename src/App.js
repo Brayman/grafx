5d3b8ca701a627c1ba1dc03fdcebc0b9dac40e28
Rakitskiy <ar.rakitskiy@gmail.com>
2021-11-13 13:46:03 +0300

    Научился рисовать график на 2 выходных

--- src/App.js
+++ src/App.js
@@ -1,104 +1,127 @@
 import logo from './logo.svg';
 import './App.css';
-const data = 
-    {
-      data: 'Ноябрь',
-        user1: [
-            11,11,10,0,0,0,11,11,10,0,0,0,11,11,10,0,0,0,11,11,10,0,0,0,11,11,10,0,0,0,11
-        ],
-        user2: [
-            0,11,11,10,0,0,0,11,11,10,0,0,0,11,11,10,0,0,0,11,11,10,0,0,0,11,11,10,0,0,0
-        ],
-        user3: [
-            0,0,11,11,10,0,0,0,11,11,10,0,0,0,11,11,10,0,0,0,11,11,10,0,0,0,11,11,10,0,0
-        ],
-        user4: [
-            0,0,0,11,11,10,0,0,0,11,11,10,0,0,0,11,11,10,0,0,0,11,11,10,0,0,0,11,11,10,0
-        ],
-        user5: [
-            10,0,0,0,11,11,10,0,0,0,11,11,10,0,0,0,11,11,10,0,0,0,11,11,10,0,0,0,11,11,10
-        ],
-        user6: [
-            11,10,0,0,0,11,11,10,0,0,0,11,11,10,0,0,0,11,11,10,0,0,0,11,11,10,0,0,0,11,11
-        ]
+
+const workers = ['user1','user2','user3','user4','user5','user6']
+
+function Month(workers, date, last) {
+    this.date = new Date(date.getFullYear(),date.getMonth(),1);
+    workers.reduce((acc,el,i) => {
+        const day = returnDay(acc)
+        this[el] = [day];
+        return day.type
+    },last)
+    const day = new Date(date.getFullYear(),date.getMonth()+1,0).getDate()
+    for (let i = 0; i < day; i++) {
+        workers.reduce((acc,el,i) => {
+            const day = returnDay(acc)
+            this[el].push(day);
+            return day.type
+
+        },this[workers[5]][i].type)
     }
 
+}
 
+function returnDay(last) {
+    switch (last) {
+        case 'first-day':
+            return {
+                type: 'second-day',
+                hours: 11
+            }
+        case 'second-day':
+            return {
+                type: 'night',
+                hours: 10
+            }
+        case 'night':
+            return {
+                type: 'first-weekend',
+                hours: 0
+            }
+        case 'first-weekend':
+            return {
+                type: 'second-weekend',
+                hours: 0
+            }
+        case 'second-weekend':
+            return {
+                type: 'first-day',
+                hours: 11
+            }
+        case 'weekend':
+            return {
+                type: 'first-day',
+                hours: 11
+            }
+        default:
+            break;
+    }
+}
 
 function App(params) {
+
   const date = new Date()
+  console.log(new Month(workers,date,'weekend'));
+  console.log(returnDay('weekend'));
   const numberDay = date.getDate()
   const drawday = (element, i) => {
     const day = new Date(date.getFullYear(),date.getMonth(),i+1).getDay()
-    console.log(day,new Date(date.getFullYear(),date.getMonth(),i+1));
+   
     if (i+1 === numberDay) {
-      return <td className={'today '+ day === 6 ? 'weekend' : ''}>{i+1}</td>
+      return <td className={`today ${day === 6 ? 'weekend' : ''}`}>{i+1}</td>
     }
-      return <td className={ day == 0 ||6 ? 'weekend' : ''}>{i+1}</td>
+      return <td className={`${ 6 == day || 0 == day ? 'weekend' : ''}`}>{i+1}</td>
   }
+  const drawGraf = (element, i) => {
+    if (i+1 === numberDay) {
+      return <td className={`${element.type} today`}>{element.hours}</td>
+    }
+      return <td className={element.type}>{element.hours}</td>
+  }
+  const month = new Month(workers,date,'weekend')
     return <table>
         <tbody>
             <tr>
                 <td>Day</td>
                 {
-                    data.user1.map(drawday)
+                    month.user1.map(drawday)
                 }
             </tr>
             <tr>
                 <td>user1</td>
                 {
-                    data.user1.map((element, i) => {
-                      if (i+1 === numberDay) {
-                        return <td className={element < 10 ? 'today none' : 'today work'}>{element}</td>
-                      }
-                        return <td className={element < 10 ? 'none' : 'work'}>{element}</td>
-                    })
+                    month.user1.map(drawGraf)
                 }
             </tr>
             <tr>
                 <td>user2</td>
                 {
-                    data.user2.map((element, i) => {
-                      if (i+1 === numberDay) {
-                        return <td className={element < 10 ? 'today none' : 'today work'}>{element}</td>
-                      }
-                        return <td className={element < 10 ? 'none' : 'work'}>{element}</td>
-                    })
+                    month.user2.map(drawGraf)
                 }
             </tr>
             <tr>
                 <td>user3</td>
                 {
-                    data.user3.map((element, i) => {
-                      if (i+1 === numberDay) {
-                        return <td className={element < 10 ? 'today none' : 'today work'}>{element}</td>
-                      }
-                        return <td className={element < 10 ? 'none' : 'work'}>{element}</td>
-                    })
+                    month.user3.map(drawGraf)
                 }
             </tr>
             <tr>
                 <td>user4</td>
                 {
-                    data.user4.map((element, i) => {
-                        return <td className={element < 10 ? 'none' : 'work'}>{element}</td>
-                    })
+                    month.user4.map(drawGraf)
                 }
             </tr>
             <tr>
                 <td>user5</td>
                 {
-                    data.user5.map((element, i) => {
-                        return <td className={element < 10 ? 'none' : 'work'}>{element}</td>
-                    })
+                    month.user5.map(drawGraf)
                 }
             </tr>
             <tr>
                 <td>user6</td>
                 {
-                    data.user6.map((element, i) => {
-                        return <td className={element < 10 ? 'none' : 'work'}>{element}</td>
-                    })
+                    month.user6.map(drawGraf)
                 }
             </tr>
         </tbody>
