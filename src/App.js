f17be2f3594cf9ec74ecae696274681e952448be
Rakitskiy <ar.rakitskiy@gmail.com>
2021-11-17 00:05:22 +0300

    Добавил роутинг

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import React from 'react';
 import './App.css';
 import Graf from './Graf';
 import pack from '../package.json'
 import { renderToString } from 'react-dom/server'
+import {
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 function App(params) {
-    return <div className='app'>
-        <Graf/>
-        <footer>
-            version: {pack.version}
-        </footer>
-    </div>
+    return (
+        <div className='app'>
+                <Switch>
+                    <Route path='/'>
+                        <Graf/>
+                    </Route>
+                </Switch>
+                
+                <footer>
+                    version: {pack.version}
+                </footer>
+            </div>
+    )
        
 }
 
