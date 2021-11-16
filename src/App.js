534ccf8ded6c3be6d1cb16c958b7a6d92fbf3d49
Rakitskiy <ar.rakitskiy@gmail.com>
2021-11-16 22:08:40 +0300

    Добавлена отрисовка контексного меню

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Graf from './Graf';
-import pack from '../package.json'
+import {name, version, author} from '../package.json'
 import { renderToString } from 'react-dom/server'
 
 function App(params) {
     return <div className='app'>
         <Graf/>
         <footer>
-            version: {pack.version}
+           <h4>{name} version: {version}</h4>
+           <h6>Created by {author}</h6>
         </footer>
     </div>
        
--- src/Graf.js
+++ src/Graf.js
@@ -80,15 +80,14 @@ function returnDay(last) {
 
 
 
-function Row({month}) {
-    const [contextMenu, setContextMenu] = useState({open: false});
+function Row({month, open}) {
     const date = new Date()
     const numberDay = date.getDate()
     const drawGraf = (element, i) => {
         if (i+1 === numberDay) {
             return <td key={`item${i}`} className={`${element.type} today`}>{element.hours}</td>
         }
-        return <td key={`item${i}`} className={element.type}>{element.hours}</td>
+        return <td key={`item${i}`} className={element.type} onClick={(e)=>open(e,element.hours)}>{element.hours}</td>
     }
     const elements = []
     for (const key in month) {
@@ -112,7 +111,7 @@ function Row({month}) {
 function Graf(params) {
     const date = new Date()
     const [month, setMonth] = useState(new Month(workers,date,'night'))
-    
+    const [contextMenu, setContextMenu] = useState({open: false, position: [0,0] ,another: ''});
     const numberDay = date.getDate()
     useEffect(()=> {
             setMonth( new Month(workers,date,'night') )   
@@ -126,25 +125,40 @@ function Graf(params) {
             return <td key={`day${i}`} className={`${ 6 === day || 0 === day ? 'weekend' : ''}`}>{i+1}</td>
         }
    
-
+    const Menu = (position) => {
+        
+        return <div className='menu' style={{left: contextMenu.position[0], top: contextMenu.position[1]}}>
+            <p>Замениться</p>
+            {contextMenu.another !== 0 ? <p>Убрать смену</p> :<p>Добавить смену</p>}
+        </div>
+    }
 
     
 
     
-    return <table>
+    return <div>
+        { contextMenu.open ? <Menu/> : null}
+        <table>
             <tbody>
-            <tr>
+                <tr>
                     <td>{new Date(month.date).toLocaleDateString('ru-ru', {month: 'long'})}</td>
                     {
                         month.user1.days.map(drawday)
                     }
                 </tr>
                 
-                <Row month={month}/>
+                <Row month={month} open={(e,item)=> {
+                    setContextMenu({
+                        open: !contextMenu.open,
+                        position: [e.target.offsetLeft,e.target.offsetTop+e.target.offsetHeight],
+                        another: item,
+                    })
+                    console.log(e);
+                }}/>
                
                 
             </tbody>
-            
         </table>
+    </div>
 }
 export default Graf
