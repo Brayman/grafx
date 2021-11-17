8d2905e87d2f3c01468c610ade2974a2094e90a1
Rakitskiy <ar.rakitskiy@gmail.com>
2021-11-17 21:41:11 +0300

    Добавил конект к БД

--- server/server.js
+++ server/server.js
@@ -2,9 +2,27 @@ const path = require('path');
 const axios = require('axios');
 const cors = require('cors');
 const express = require('express');
+const config = require('../config.json');
+const mongoose = require('mongoose')
+const uri = process.env.MONGODB_URI;
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+async function start() {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+});
+  } catch(e) {
+    console.log("server error", e.message);
+    process.exit(1)
+  }
+}
 
 const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.static(buildPath));
@@ -14,6 +32,4 @@ app.get('/jobs', async (req, res) => {
     res.send('job');
 });
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+start()
