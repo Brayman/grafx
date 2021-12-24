d8fb4cb9dbab2cadfe2503be0c94c8d14d9399f8
Rakitskiy <ar.rakitskiy@gmail.com>
2021-12-24 11:02:50 +0300

    Сделал отрисовку команды

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,9 +1,15 @@
+import { useEffect, useState } from "react";
 import { FaTelegram } from "react-icons/fa";
 import { useDispatch } from "react-redux";
-import { fetch_logout } from "../redux/actions/user"
+import {
+    fetch_logout,
+    fetch_get_team
+} from "../redux/actions"
 
 function Profile({user}) {
+    const [team, setTeam] = useState();
     const dispatch = useDispatch();
+    useEffect(() => setTeam(dispatch(fetch_get_team("Brayman"))),[])
     if (user.login) {
         return (
             <section>
@@ -17,7 +23,17 @@ function Profile({user}) {
                 <p>
                     <FaTelegram color='#0a96d5'/> {user.telegram.username}
                 </p>
-                
+                <div>
+                {/* <select onChange={(e) => { console.log(e.target.value); setWorkers([...workers,e.target.value])}}>
+                    {worke.map((item, i, arr) => {
+                        return <option>{item}</option>
+                    })}
+                </select> 
+                <div>
+                    { team[0] ? team.map((item, i, arr) => {return <div>{item}</div>}) : null}
+                </div>*/}
+                <button>Выбрать</button>
+            </div>
             </section>
         )
     } else {
