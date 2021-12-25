a555c33edc0172aa25c8ea41cca3f6f1f0a00c1a
Rakitskiy <ar.rakitskiy@gmail.com>
2021-12-26 02:51:56 +0300

    Сделал добавление людей в команду с аккаунтами и без

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,23 +1,21 @@
 import { useEffect, useState } from "react";
 import { FaTelegram } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
-import { fetch_logout, fetch_get_team, fetch_get_users } from "../redux/actions"
+import { fetch_logout, fetch_get_team, fetch_get_users, fetch_save_team } from "../redux/actions";
+import TeamPanel from "./TeamPanel";
+import '../css/profile.css';
 
 function Profile() {
     const [workers, setWorkers] = useState([])
-    useEffect(() => {
-        dispatch(fetch_get_team("Brayman"))
-        dispatch(fetch_get_users())
-    },[])
     const dispatch = useDispatch();
     const {user, users, team} = useSelector((state) => state)
+    useEffect(() => {
+        dispatch(fetch_get_team(user.login))
+        dispatch(fetch_get_users())
+    },[])
+    
 
-    function filter(el, i ,arr) {
-        console.log(this);
-        if (el.login == this.target) {
-            setWorkers([...workers,el])
-        } 
-    }
+    
 
     if (user.login) {
         return (
@@ -32,14 +30,7 @@ function Profile() {
                 <p>
                     <FaTelegram color='#0a96d5'/> {user.telegram.username}
                 </p>
-                <div>
-                    {team.map((item, i, arr) => {return <div>{item}</div>})}
-                </div>
-                <select onChange={(e) => users.find(filter, {target: e.target.value})}>
-                    {users.map((item, i, arr) => {
-                        return <option>{item.first_name||item.login}</option>
-                    })}
-                </select>
+                <TeamPanel team={team} users={users} saveTeam={(team) => dispatch(fetch_save_team(team))}/>
             </section>
         )
     } else {
